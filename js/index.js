909bd119994a55324fb0dbb7f4b27d77a047986c
Ash Williams <ihor.khrystiuk@gmail.com>
2018-02-22 19:51:04 +0000

    add code for popup modal form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Map
 var map;
 
 function initMap() {
@@ -13,7 +14,7 @@ function initMap() {
     position: nerds,
     map: map,
     title: 'Студия дизайна Нёрдс!',
-    icon: 'http://localhost:8080/nerds/img/map-marker.png'
+    icon: 'http://127.0.0.1:8080/img/map-marker.png'
   });
 
   var contentString = '<b class="contacts__title">NЁRDS DESIGN STUDIO</b>' +
@@ -27,3 +28,49 @@ function initMap() {
     infowindow.open(map, marker);
   });
 }
+
+// Popup
+var popup = document.querySelector(".modal-form");
+var writeUs = document.querySelector(".btn_contacts");
+var closePopup = document.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+
+writeUs.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  popup.classList.toggle("modal-show");
+  login.focus();
+});
+
+closePopup.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (!login.value) {
+    login.classList.add("modal-form__required-value");
+  }
+
+  if (!email.value) {
+    email.classList.add("modal-form__required-value");
+  }
+
+  if (!text.value) {
+    text.classList.add("modal-form__required-value");
+  }
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
